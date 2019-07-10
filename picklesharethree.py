ba9122af195250d4005100aa579d292d0b72e6cb
cabinpressure <chad.xyz.123@gmail.com>
2019-07-10 16:19:43 -0500

    Update picklesharethree.py

--- picklesharethree.py
+++ picklesharethree.py
@@ -1,35 +1,7 @@
 #!/usr/bin/env python
 
-""" PickleShare - a small 'shelve' like datastore with concurrency support
+""" 
 
-Like shelve, a PickleShareDB object acts like a normal dictionary. Unlike
-shelve, many processes can access the database simultaneously. Changing a
-value in database is immediately visible to other processes accessing the
-same database.
-
-Concurrency is possible because the values are stored in separate files. Hence
-the "database" is a directory where *all* files are governed by PickleShare.
-
-Example usage::
-
-    from pickleshare import *
-    db = PickleShareDB('~/testpickleshare')
-    db.clear()
-    print "Should be empty:",db.items()
-    db['hello'] = 15
-    db['aku ankka'] = [1,2,313]
-    db['paths/are/ok/key'] = [1,(5,46)]
-    print db.keys()
-    del db['aku ankka']
-
-This module is certainly not ZODB, but can be used for low-load
-(non-mission-critical) situations where tiny code size trumps the
-advanced features of a "real" object database.
-
-Installation guide: pip install pickleshare
-
-Author: Ville Vainio <vivainio@gmail.com>
-License: MIT open source license.
 
 """
 
@@ -38,28 +10,16 @@ from __future__ import print_function
 
 __version__ = "0.7.5"
 
-try:
-    from pathlib import Path
-except ImportError:
-    # Python 2 backport
-    from pathlib2 import Path
 
-import os,stat,time
-try:
-    import collections.abc as collections_abc
-except ImportError:
-    import collections as collections_abc
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
+import os
+import stat
+import time
+
+from pathlib import Path
+import collections.abc as collections_abc
+import pickle
 import errno
 import sys
-
-if sys.version_info[0] >= 3:
-    string_types = (str,)
-else:
-    string_types = (str, unicode)
 
 def gethashfile(key):
     return ("%02x" % abs(hash(key) % 256))[-2:]
